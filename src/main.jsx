72582096f665441d2c8f5e09309fb3a0c85c739c
Bhavya <bhavyavadhan4@gmail.com>
2024-07-21 01:48:06 +0530

    bas

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,26 @@ import About from "./Components/About.jsx"
 import Resume from "./Components/Resume.jsx"
 import Works from "./Components/Works.jsx"
 import Contact from "./Components/Contact.jsx"
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, Routes, BrowserRouter } from "react-router-dom"
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<About />} />
-      <Route path="Resume" element={<Resume />} />
-      <Route path="Works" element={<Works />} />
-      <Route path="Contact" element={<Contact />} />
-    </Route>
-  )
-)
+const Router = () => {
+  return <>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="" element={<About />} />
+        <Route path="Resume" element={<Resume />} />
+        <Route path="Works" element={<Works />} />
+        <Route path="Contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </>
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+);
